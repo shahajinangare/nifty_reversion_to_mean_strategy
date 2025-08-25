5007101e4b3a64ce40d5b73a45a3f1fb5c07af3a
Shahaji Nangare <shahaji.nangare@gmail.com>
2025-08-25 12:12:58 +0530

    update

--- niffy_reversiontomean_strategy_app.py
+++ niffy_reversiontomean_strategy_app.py
@@ -1,6 +1,3 @@
-import pandas as pd 
-import numpy as np 
-from collections import defaultdict 
 from nifty.service import ClosingPricesService
 from nifty.strategy.reversiontomean import ReversionToMean
 import streamlit as st
